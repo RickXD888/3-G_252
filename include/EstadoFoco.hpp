084f8068b3ac8f1cdd99abd4fefa91c8fcddd036
Rick <a24310380@live.ceti.mx>
2025-09-13 02:03:34 +0000

    practica 2

--- include/EstadoFoco.hpp
+++ include/EstadoFoco.hpp
@@ -1,2 +1,3 @@
 #pragma once
+
 typedef bool  EstadoFoco;
---
+++ src/memoria.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int variable = 7;
+    cout << "Direccion: " << &variable << endl;
+    cout << "Valor: " << variable << endl;
+    cout << "Indireccion: " << *(&variable) << endl;
+    cout << "int: " << sizeof(int) << endl;
+    cout << "bool: " << sizeof(bool) << endl;
+    cout << "long: " << sizeof(long) << endl;
+    cout << "char: " << sizeof(char) << endl;
+    cout << "double: " << sizeof(double) << endl;
+    cout << "byte: " << sizeof(byte) << endl;
+    cout << "float: " << sizeof(float) << endl;
+
+    void *direccion = malloc(4);
+    cout << "direccion: " << direccion << endl;
+    cout << "tamaño: " << sizeof(direccion) << endl;
+    cout << "direccion puntero: " << &direccion << endl;
+
+    int prueba = 64;
+    char casteo = (char) prueba;
+    cout << "Resultado: " << casteo<<endl;
+
+    return 0;
+}
